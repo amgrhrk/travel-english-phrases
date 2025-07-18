e3afd9c04842928aac44db9d12436d23c15b0618
amgrhrk <67768560+amgrhrk@users.noreply.github.com>
2025-07-17 21:16:31 -0400

    Implement search function

--- scripts/index.js
+++ scripts/index.js
@@ -53,34 +53,45 @@ appUI.create('phrase', html `
 	<div class="travel-english">
 		<div class="top">
 			<div class="left"></div>
-			<div class="center">旅游英语100句</div>
+			<div class="center">
+				<div class="title">旅游英语100句</div>
+				<input class="search-bar hide">
+			</div>
 			<div class="right">搜索</div>
 		</div>
 		<div class="scroll maximized"></div>
-
 	</div>
 `, phraseScreen => {
     const phraseContainer = phraseScreen.element.querySelector('.scroll');
     const searchButton = phraseScreen.element.querySelector('.right');
+    const searchBar = phraseScreen.element.querySelector('.search-bar');
+    const centerArea = phraseScreen.element.querySelector('.center');
+    const appTitle = phraseScreen.element.querySelector('.title');
+    const input = phraseScreen.element.querySelector('input');
     const audio = new Audio();
-    const removeHighLightFromAllItems = () => {
-        const items = phraseContainer.querySelectorAll('.item.clicked');
-        for (const item of items) {
-            item.classList.remove('clicked');
+    let highlightedElement = null;
+    let items = null;
+    audio.addEventListener('ended', () => {
+        if (highlightedElement) {
+            highlightedElement.classList.remove('clicked');
+            highlightedElement = null;
         }
-    };
-    audio.addEventListener('ended', () => removeHighLightFromAllItems());
-    const appendPhrase = (phrase) => {
+    });
+    const appendPhrase = (english, chinese, audioBase64) => {
         const item = TravelEnglishUI.createElement(html `
 			<div class="item">
-				<div class="english">${phrase.english}</div>
-				<div class="chinese">${phrase.chinese}</div>
+				<div class="english">${english}</div>
+				<div class="chinese">${chinese}</div>
+				<div class="audio" style="display: none;">${audioBase64}</div>
 			</div>
 		`);
         item.addEventListener('click', () => {
-            removeHighLightFromAllItems();
+            if (highlightedElement) {
+                highlightedElement.classList.remove('clicked');
+            }
+            highlightedElement = item;
             item.classList.add('clicked');
-            audio.src = `${phrase.audio}`;
+            audio.src = item.querySelector('.audio').innerText;
             audio.play();
         });
         phraseContainer.appendChild(item);
@@ -89,24 +100,47 @@ appUI.create('phrase', html `
         try {
             const lines = (await (await fetch('./phrases/1.txt')).text()).split('\n');
             for (let i = 0; i < lines.length; i += 4) {
-                appendPhrase({
-                    english: lines[i],
-                    chinese: lines[i + 1],
-                    audio: lines[i + 2]
-                });
+                appendPhrase(lines[i], lines[i + 1], lines[i + 2]);
             }
         }
         catch (err) {
-            appendPhrase({
-                english: `Error: ${err}`,
-                chinese: '软件错误',
-                audio: ''
-            });
+            appendPhrase(`${err}`, '软件错误', '');
         }
     };
-    // searchButton.addEventListener('click', () => {
-    // 	phraseContainer.focus()
-    // })
+    searchButton.addEventListener('click', () => {
+        searchBar.classList.toggle('hide');
+        appTitle.classList.toggle('hide');
+        centerArea.classList.toggle('fill');
+        if (searchButton.innerText === '搜索') {
+            searchButton.innerText = '关闭';
+            input.focus();
+        }
+        else {
+            searchButton.innerText = '搜索';
+            input.value = '';
+            if (items) {
+                for (const item of items) {
+                    item.classList.remove('hide');
+                }
+            }
+        }
+    });
+    input.addEventListener('input', () => {
+        if (!items) {
+            items = [...phraseContainer.querySelectorAll('.item')];
+        }
+        const value = input.value.trim();
+        for (const item of items) {
+            const english = item.querySelector('.english');
+            const chinese = item.querySelector('.chinese');
+            if (english.innerText.toLowerCase().includes(value.toLowerCase()) || chinese.innerText.includes(value)) {
+                item.classList.remove('hide');
+            }
+            else {
+                item.classList.add('hide');
+            }
+        }
+    });
     loadPhrases();
 });
 appUI.show('phrase');
